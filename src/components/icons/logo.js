77ae3b0e78648e16ca13c923d88ef8fe38e13d6a
kibalion2427 <roger1994a@gmail.com>
2020-02-20 19:38:02 -0500

    svg icon style

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,15 +1,10 @@
 import React from "react";
 
 const IconLogo = () => (
-  <svg
-    // height="1995"
-    viewBox="0 0 161.67 129"
-    width="2500"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <svg viewBox="0 0 161.67 129" xmlns="http://www.w3.org/2000/svg">
     <path
       d="m88.33 0-47.66 41.33-40.67 73h36.67zm6.34 9.67-20.34 57.33 39 49-75.66 13h124z"
-      fill="#0072c6"
+      fill="#0a192f"
     />
   </svg>
 );
